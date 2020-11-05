b64b02a4d4f04ce173d583dff9fb43a184051508
Gustav-prog <73692613+Gustav-prog@users.noreply.github.com>
2020-11-05 11:21:38 +0100

    changed superclass

---
+++ src/dopeAgile/Rouge.java
@@ -0,0 +1,18 @@
+
+package dopeAgile;
+
+public class Rouge extends Character {
+       
+public Rouge() {
+    
+    super();
+    this.initiative = 7;
+    this.endurance = 5;
+    this.attack = 5;
+    this.agility = 7;  
+
+}
+
+
+}
+
