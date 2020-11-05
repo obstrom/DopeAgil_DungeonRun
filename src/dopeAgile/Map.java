32760d00985c05307939e30344eacbb2f6f602d0
Oscar <oscar@obstrom.com>
2020-11-05 10:34:07 +0100

    Reworked Map class

--- src/dopeAgile/Map.java
+++ src/dopeAgile/Map.java
@@ -1,16 +1,33 @@
 package dopeAgile;
 
 public class Map {
-    
-    public enum mapSize { SMALL, MEDIUM, LARGE};
-    
-    private mapSize[][] board;
-    
-    public smallMap(){
-      board = new mapSize[4][4];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = mapSize.SMALL;
+    private mapSize mapSize;
+
+    Map(mapSize sizeEnum) {
+        createMap(sizeEnum.getSize());
+    }
+
+    public enum mapSize {
+        SMALL(4),
+        MEDIUM(5),
+        LARGE(8);
+
+        private final int size;
+
+        mapSize(final int newSize) {
+            size = newSize;
+        }
+
+        int getSize() {
+            return this.size;
+        }
+    }
+
+    public void createMap(int size) {
+        Room[][] mapArray = new Room[size][size];
+        for (int i = 0; i < mapArray.length; i++) {
+            for (int j = 0; j < mapArray[i].length; j++) {
+                mapArray[i][j] = new Room();
             }
         }
     }
