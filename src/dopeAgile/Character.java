2d1e47587b2f0921928a83779ba18657e4b3224e
Gustav-prog <73692613+Gustav-prog@users.noreply.github.com>
2020-11-05 14:07:20 +0100

    update to main

--- src/dopeAgile/Character.java
+++ src/dopeAgile/Character.java
@@ -10,12 +10,17 @@ public class Character {
     int positionX;
     int positionY;
     private String name;
+    int points;
+    int playerId;
+    int idGenerator;
  
 public Character() {
     
    this.positionX = 0;
    this.positionY = 0;
    this.name = name;
+   this.points = 0;
+   this.playerId = idGenerator;
     
 }    
 
