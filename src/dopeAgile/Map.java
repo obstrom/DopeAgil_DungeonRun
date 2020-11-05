e8183b7f84e434ba98a4186e4a74d3a1bd65af72
PaulBlumenberg <72750410+PaulBlumenberg@users.noreply.github.com>
2020-11-05 10:03:45 +0100

    Updated map in progress

--- src/dopeAgile/Map.java
+++ src/dopeAgile/Map.java
@@ -3,4 +3,16 @@ package dopeAgile;
 
 public class Map {
     
+    public enum mapSize { SMALL, MEDIUM, LARGE};
+    
+    private mapSize[][] board;
+    
+    public smallMap(){
+      board = new mapSize[4][4];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = mapSize.SMALL;
+            }
+        }
+    }
 }
