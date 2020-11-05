81ff59a5858943767317ac9eae1c804cbdab5d54
Alexander <Agieson@hotmail.com>
2020-11-05 11:32:22 +0100

    Knight and Wizard class added

---
+++ src/dopeAgile/Wizard.java
@@ -0,0 +1,22 @@
+
+package dopeAgile;
+
+
+public class Wizard extends Character {
+    
+    
+    
+    
+    public Wizard(){
+        
+        super();
+        initiative = 6;
+        endurance = 4;
+        attack = 9;
+        agility = 5;
+    }
+    
+    public void specialAbility() {
+        
+    }
+}
