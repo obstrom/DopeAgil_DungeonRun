558230438f716e08c910c6dd1dc2c67a4ec2100e
Alexander <Agieson@hotmail.com>
2020-11-06 13:32:00 +0100

    Troll class created

---
+++ src/dopeAgile/Troll.java
@@ -0,0 +1,17 @@
+
+package dopeAgile;
+
+
+public class Troll extends Monster {
+    
+    public Troll(){
+        
+        super();
+        this.initiative = 2;
+        this.endurance = 4;
+        this.attack = 7;
+        this.agility = 2;
+        this.common = 0.05;
+    }
+    
+}
