5097b7cd405aa1391e7a8c7a9606a090308d2ece
Oscar <oscar@obstrom.com>
2020-11-06 11:45:20 +0100

    Started on the loop itself

--- src/dopeAgile/GameLoop.java
+++ src/dopeAgile/GameLoop.java
@@ -3,6 +3,8 @@ package dopeAgile;
 public class GameLoop {
     private Map loadedMap;
     private Character loadedCharacter;
+    private Room currentRoom;
+    private int loopIteration = 0;
 
     GameLoop(Map loadedMap, Character loadedCharacter) {
         this.loadedMap = loadedMap;
@@ -10,8 +12,16 @@ public class GameLoop {
         loop();
     }
 
-    public static void loop() {
+    public void loop() {
+        while (true) {
+            if (loopIteration == 0) {
+                currentRoom = loadedMap.getSpawnRoom();
+            }
 
+
+
+            break; // Remove when exit map is implemented
+        }
     }
 
 }
