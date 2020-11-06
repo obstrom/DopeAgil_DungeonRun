e2497e3a5b04cba96ed9db4528e0434dc0fd5717
Alexander <Agieson@hotmail.com>
2020-11-06 13:30:12 +0100

    Sub classes for Monster created

---
+++ src/dopeAgile/Monster.java
@@ -0,0 +1,55 @@
+
+package dopeAgile;
+
+
+public class Monster {
+    
+    int initiative;
+    int endurance;
+    int attack;
+    int agility;
+    double common;
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public double getCommon() {
+        return common;
+    }
+
+    public void setCommon(double common) {
+        this.common = common;
+    }
+    
+    
+    
+}
