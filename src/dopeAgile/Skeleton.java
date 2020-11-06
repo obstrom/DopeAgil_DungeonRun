6e1e312f04abf82b67b464f576a7389a851bf00d
Alexander <Agieson@hotmail.com>
2020-11-06 13:32:40 +0100

    Skeleton class created

---
+++ src/dopeAgile/Skeleton.java
@@ -0,0 +1,17 @@
+
+package dopeAgile;
+
+
+public class Skeleton extends Monster {
+    
+    public Skeleton(){
+        
+        super();
+        this.initiative = 4;
+        this.endurance = 2;
+        this.attack = 2;
+        this.agility = 3;
+        this.common = 0.15;
+    }
+    
+}
