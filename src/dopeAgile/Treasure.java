9fda799c72fce5ba6b5cfd47e86afc220f370237
PaulBlumenberg <72750410+PaulBlumenberg@users.noreply.github.com>
2020-11-06 14:08:10 +0100

    uppdated treasure class

--- src/dopeAgile/Treasure.java
+++ src/dopeAgile/Treasure.java
@@ -21,28 +21,40 @@ public class Treasure {
         final int goldJewelryPoints = 10;
         final int gemStonePoints = 14;
         final int smallChestPoints = 20;
+       
         */
-        int spawnCoin = ThreadLocalRandom.current().nextInt(1, 100);
+      int spawnCoin = 0;
+      int spawnMoneyPouch = 0;
+      int spawnGoldJewelry = 0;
+      int spawnGemstone = 0;
+      int spawnSmallChest = 0;
+      
+        int spawnTreasure = ThreadLocalRandom.current().nextInt(1, 100);
+        spawnTreasure = spawnCoin;
         if (spawnCoin <= 40) {
             
         }
         
-        int spawnMoneyPouch = ThreadLocalRandom.current().nextInt(1, 100);
+        //int spawnMoneyPouch = ThreadLocalRandom.current().nextInt(1, 100);
+        spawnTreasure = spawnMoneyPouch;
         if (spawnMoneyPouch <= 20) {
 
         }
 
-        int spawnGoldJewelry = ThreadLocalRandom.current().nextInt(1, 100);
+        //int spawnGoldJewelry = ThreadLocalRandom.current().nextInt(1, 100);
+        spawnTreasure = spawnGoldJewelry;
         if (spawnGoldJewelry <= 15) {
 
         }
         
-        int spawnGemstone = ThreadLocalRandom.current().nextInt(1, 100);
+        //int spawnGemstone = ThreadLocalRandom.current().nextInt(1, 100);
+        spawnTreasure = spawnGemstone;
         if (spawnGemstone <= 10) {
 
         }
         
-        int spawnSmallChest = ThreadLocalRandom.current().nextInt(1, 100);
+        //int spawnSmallChest = ThreadLocalRandom.current().nextInt(1, 100);
+        spawnTreasure = spawnSmallChest;
         if (spawnSmallChest <= 5) {
 
         }
