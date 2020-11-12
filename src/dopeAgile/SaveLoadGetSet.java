b380fa99446c695f8601b200c92731082ac5c583
Dogginator <douglas.kvarnstrom91@gmail.com>
2020-11-12 10:03:30 +0100

    Test

---
+++ src/dopeAgile/SaveLoadGetSet.java
@@ -0,0 +1,14 @@
+
+package dopeAgile;
+
+public class SaveLoadGetSet {
+   private int agility;
+   private int points;
+   private String name;
+   
+   
+}
+public SaveLoadGetSet(String name, int points, int agility){
+
+}
+
