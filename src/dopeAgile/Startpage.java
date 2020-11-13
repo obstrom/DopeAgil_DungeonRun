55daffae282ad2970de0ddab50f01b2297433644
PaulBlumenberg <72750410+PaulBlumenberg@users.noreply.github.com>
2020-11-13 11:38:14 +0100

    startpage added

---
+++ src/dopeAgile/Startpage.java
@@ -0,0 +1,44 @@
+
+package dopeAgile;
+import java.util.Scanner;
+public class Startpage {
+    
+    public static final void Startpage(){
+        System.out.println("    ___                                                       ");
+        System.out.println("   /   \\_   _ _ __   __ _  ___  ___  _ __    _ __ _   _ _ __  ");
+        System.out.println("  / /\\ / | | | '_ \\ / _` |/ _ \\/ _ \\| '_ \\  | '__| | | | '_ \\ ");
+        System.out.println(" / /_//| |_| | | | | (_| |  __/ (_) | | | | | |  | |_| | | | |");
+        System.out.println("/___,'  \\__,_|_| |_|\\__, |\\___|\\___/|_| |_| |_|   \\__,_|_| |_|");
+        System.out.println("                    |___/ ");
+        System.out.println("");
+        System.out.println("");
+                
+
+        System.out.println("                            ,-.     ");
+        System.out.println("       ___,---.__          /'|`\\          __,---,___         ");
+        System.out.println("    ,-'    \\`    `-.____,-'  |  `-.____,-'    //    `-.      ");
+        System.out.println("  ,'        |           ~'\\     /`~           |        `.    ");
+        System.out.println(" /      ___//              `. ,'          ,  , \\___      \\   ");
+        System.out.println("|    ,-'   `-.__   _         |        ,    __,-'   `-.    |  ");
+        System.out.println("|   /          /\\_  `   .    |    ,      _/\\          \\   |  ");
+        System.out.println("\\  |           \\ \\`-.___ \\   |   / ___,-'/ /           |  /  ");
+        System.out.println(" \\  \\           | `._   `\\\\  |  //'   _,' |           /  /   ");
+        System.out.println("  `-.\\         /'  _ `---'' , . ``---' _  `\\         /,-'    ");
+        System.out.println("     ``       /     \\    ,='/ \\`=.    /     \\       ''       ");
+        System.out.println("             |__   /|\\_,--.,-.--,--._/|\\   __|               ");
+        System.out.println("             /  `./  \\\\`\\ |  |  | /,//' \\,'  \\               ");
+        System.out.println("            /   /     ||--+--|--+-/-|     \\   \\              ");
+        System.out.println("           |   |     /'\\_\\_\\ | /_/_/`\\     |   |             ");
+        System.out.println("            \\   \\__, \\_     `~'     _/ .__/   /            ");
+        System.out.println("             `-._,-'   `-._______,-'   `-._,-'");
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("          -*-*- Enter anything to continue -*-*-");
+        
+        Scanner sc = new Scanner(System.in);
+       
+        String n = sc.nextLine();
+}
+    
+}
