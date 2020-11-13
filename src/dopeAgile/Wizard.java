ddbd6f5880877496fa67144510ab7e7d6d8b3582
PaulBlumenberg <72750410+PaulBlumenberg@users.noreply.github.com>
2020-11-13 16:03:37 +0100

    Wizard

--- src/dopeAgile/Wizard.java
+++ src/dopeAgile/Wizard.java
@@ -26,40 +26,56 @@ public class Wizard extends Character {
         return "";
     }
 
-    /*
-    @Override
-    public String getAttackMessage(Monster monster) {
+    
+    
+    
 
-        if (monster instanceof Orc) {
+    @Override
+    public String getAttackHitMessage(Creature creature) {
+        
+        Monster monster = (Monster) creature;
+        
+        if (Monster instanceof Orc) {
 
             // Kod Orc
             int rand = Utility.throwSixSidedDie(); // random number 1 <-> 6
 
             if (rand < 4) {
-                return "Här är Orc attack 1";
+                return "Trollkarlen säger en besvärjelse och en magisk pil skapas och avfyras, den träffar orcen i knäet.";
             } else {
-                return "Här är Orc attack 2";
+                return "Trollkarlen tar sin stav med två händer och svingar underifrån. Den träffar undersidan av orcens haka och orcen faller till marken ";
             }
 
         } else if (monster instanceof Spider) {
+            int rand = Utility.throwSixSidedDie(); // random number 1 <-> 6
 
+            if (rand < 4) {
+                return "Trollkarlen skapar ett eldklot och avfyrar det mot spindeln. Eldklotet träffar, spindeln skriker i smärta.";
+            } else {
+                return "Trollkarlen tar sin stav med två händer över huvudet och slår ner i mitten av spindelns kropp så att spindel faller platt ner på marken.";
+            }
 
         } else if (monster instanceof Skeleton) {
+            int rand = Utility.throwSixSidedDie(); // random number 1 <-> 6
 
+            if (rand < 4) {
+                return "Trollkarlen slår ner sin stav i marken och avfyrar en blixt från sin trollstav, den träffar och skelettet flyger bakåt. ";
+            } else {
+                return "Trollkarlen tar sin stav med två händer och träffar skelettets huvud så att det flyger iväg. Skelettet springer iväg för hämta sitt huvud.";
+            }
 
         } else if (monster instanceof Troll ) {
+            int rand = Utility.throwSixSidedDie(); // random number 1 <-> 6
 
+            if (rand < 4) {
+                return "Trollkarlen säger en besvärjelse och en rot växer ut från marken som sedan med sin vassa ände hugger trollet i benet.";
+            } else {
+                return "Trollkarlen tar sin stav med två händer och använder sin stav som ett spjut och skjuter in änden av staven i trollets bröst så att han faller till marken. ";
+            }
 
         } else {
-            return "";
+            return " ";
         }
-
-    }
-    */
-
-    @Override
-    public String getAttackHitMessage() {
-        return "Hit";
     }
 
     @Override
